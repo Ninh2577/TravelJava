e7f78c3331c1365fd9b87cd07890b23f4c45032b
Ninh2577 <hoangninh2577@gmail.com>
2024-11-07 16:00:56 +0700

    fix

--- src/main/java/com/example/Repository/DanhGiaRepository.java
+++ src/main/java/com/example/Repository/DanhGiaRepository.java
@@ -1,9 +1,14 @@
 package com.example.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.Entity.DanhGia;
 
 public interface DanhGiaRepository extends JpaRepository<DanhGia, Integer>{
-
+	@Query("SELECT d FROM DanhGia d WHERE d.tour.id = :idTour")
+    List<DanhGia> findByDanhGiaId(@Param("idTour") Integer idTour);
 }
