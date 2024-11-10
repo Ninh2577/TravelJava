08298585f92b48b1425b427180d8264cf723140c
ket36 <dinhvanket36@gmail.com>
2024-11-10 13:28:14 +0700

    .

--- src/main/java/com/example/Controller/DoanhThuController.java
+++ src/main/java/com/example/Controller/DoanhThuController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.web.bind.annotation.*;
 
 import com.example.DTO.DoanhThuDTO;
+import com.example.DTO.DoanhThuNamDTO;
+import com.example.DTO.DoanhThuNgayDTO;
+import com.example.DTO.DoanhThuThangDTO;
 import com.example.service.DoanhThuService;
 
 @RestController
@@ -22,8 +25,23 @@ public class DoanhThuController {
     DoanhThuService doanhThuService;
 
     // API để lấy doanh thu theo năm
-    @GetMapping("/doanh-thu")
-    public List<DoanhThuDTO> getDoanhThuByYearRange(
+    // @GetMapping("/doanh-thu")
+    // public List<DoanhThuDTO> getDoanhThuByYearRange(
+    // @RequestParam("startYear") int startYear,
+    // @RequestParam("endYear") int endYear) {
+
+    // System.out.println("Start Year: " + startYear + ", End Year: " + endYear);
+    // return doanhThuService.getDoanhThuByYear(startYear, endYear);
+    // }
+
+    // Endpoint lấy danh sách các năm
+    @GetMapping("/doanh-thu/years")
+    public List<Integer> getAllDistinctYears() {
+        return doanhThuService.getAllDistinctYears();
+    }
+
+    @GetMapping("/doanh-thu-nam")
+    public List<DoanhThuNamDTO> getDoanhThuTheoKhoangNam(
             @RequestParam("startYear") int startYear,
             @RequestParam("endYear") int endYear) {
 
@@ -31,9 +49,22 @@ public class DoanhThuController {
         return doanhThuService.getDoanhThuByYear(startYear, endYear);
     }
 
-    // Endpoint lấy danh sách các năm
-    @GetMapping("/years")
-    public List<Integer> getAllDistinctYears() {
-        return doanhThuService.getAllDistinctYears();
+
+    @GetMapping("/doanh-thu-thang")
+    public List<DoanhThuThangDTO> getDoanhThuTheoThangTrongNam(
+            @RequestParam("year") int year) {
+    
+        System.out.println("Year: " + year);
+        return doanhThuService.getDoanhThuByMonth(year);
     }
+
+    @GetMapping("/doanh-thu-ngay")
+    public List<DoanhThuNgayDTO> getDoanhThuTheoNgayTrongNam(
+            @RequestParam("year") int year,
+            @RequestParam("month") int month) {
+    
+            System.out.println("Year: " + year + ", Month: " + month);
+            return doanhThuService.getDoanhThuByDay(year, month);
+    }
+    
 }
--- src/main/java/com/example/service/DoanhThuService.java
+++ src/main/java/com/example/service/DoanhThuService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.DTO.DoanhThuDTO;
+import com.example.DTO.DoanhThuNamDTO;
+import com.example.DTO.DoanhThuNgayDTO;
+import com.example.DTO.DoanhThuThangDTO;
 import com.example.Repository.DoanhThuRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +16,51 @@ public class DoanhThuService {
     @Autowired
     private DoanhThuRepository doanhThuRepository;
 
+    //lấy năm đổ cbo
     public List<Integer> getAllDistinctYears() {
         return doanhThuRepository.findAllDistinctYears();
     }
 
-    // Lấy doanh thu theo năm
-    public List<DoanhThuDTO> getDoanhThuByYear(int startYear, int endYear) {
+    //doanh thu theo năm 
+    public List<DoanhThuNamDTO> getDoanhThuByYear(int startYear, int endYear) {
 
-        return doanhThuRepository.findDoanhThuByYearRange(startYear, endYear);
+        return doanhThuRepository.findDoanhThuByYearRange1Dtos(startYear, endYear);
+    }
+
+    public List<DoanhThuThangDTO> getDoanhThuByMonth(int year) {
+        return doanhThuRepository.findDoanhThuByMonthInYear(year);
+    }
+
+    public List<DoanhThuNgayDTO> getDoanhThuByDay(int year, int month) {
+        return doanhThuRepository.findDoanhThuByDay(year,month);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Lấy doanh thu theo năm
+    // public List<DoanhThuDTO> getDoanhThuByYear(int startYear, int endYear) {
+
+    //     return doanhThuRepository.findDoanhThuByYearRange(startYear, endYear);
+    // }
+
+
+
+ 
+
+
+    public List<DoanhThuNamDTO> getDoanhThuByYear2(int year) {
+        return doanhThuRepository.findDoanhThuByYearRangeAndMonth(year);
     }
 }
