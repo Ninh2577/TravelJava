daa75b2fc315a84eb97775a67ed40a63fe058596
ket36 <dinhvanket36@gmail.com>
2024-11-24 19:33:18 +0700

    Update MailerService.java

--- src/main/java/com/example/service/MailerService.java
+++ src/main/java/com/example/service/MailerService.java
@@ -30,7 +30,7 @@ public class MailerService {
 		list.add(mailModel);
 	}
 
-	@Scheduled(fixedRate = 1000)
+	@Scheduled(fixedRate = 10000)
 	public void run() {
 		System.out.println("send mail");
 		while (!list.isEmpty()) {
